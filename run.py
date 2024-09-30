17f9c64fcc3c4e69abdcc1ad492f606b9b0725ec
mavillan <nallivam@gmail.com>
2024-09-30 17:00:33 -0500

    updates run script

--- run.py
+++ run.py
@@ -3,13 +3,13 @@ from pathlib import Path
 import subprocess
 import typer
 
-def remove_cell_outputs(notebook_path:str) -> None:
+def remove_output_cells(notebook_path:str) -> None:
     command = f"jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace {notebook_path}"
     process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-    output, error = process.communicate()
+    output, _ = process.communicate()
     print(output)
     
-def run_notebook(notebook_path:str, overwrite:bool = False) -> None:
+def run_notebook(notebook_path:str, overwrite:bool = True) -> None:
     notebook_path = Path(notebook_path)
     workdir = notebook_path.parent
     if not overwrite:
@@ -17,17 +17,14 @@ def run_notebook(notebook_path:str, overwrite:bool = False) -> None:
         output_path.mkdir(parents=True, exist_ok=True)
         output_path = output_path / notebook_path.name
     else:
-        output_path = workdir / f".tmp-{notebook_path.name}"
+        output_path = notebook_path
 
-    remove_cell_outputs(str(notebook_path))
+    remove_output_cells(str(notebook_path))
     pm.execute_notebook(
         input_path=str(notebook_path), 
         output_path=str(output_path),
         cwd=str(workdir),
     )
-
-    if overwrite:
-        output_path.rename(notebook_path.name)
 
 if __name__ == "__main__":
     typer.run(run_notebook)
