bc98e1a6d918c29619964e3195e12b4e0db2673a
Martín Villanueva <nallivam@gmail.com>
2024-11-28 14:16:55 -0500

    nn-mlp ready to run

--- exec.sh
+++ exec.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
-python run.py train/nn-mlp.ipynb --fs-type full
-python run.py train/nn-mlp.ipynb --fs-type fsv2
-python run.py train/nn-mlp.ipynb --fs-type fsv3
-python run.py train/nn-mlp.ipynb --fs-type fsv4
-python run.py train/nn-mlp.ipynb --fs-type fsv23
-python run.py train/nn-mlp.ipynb --fs-type fsv24
-python run.py train/nn-mlp.ipynb --fs-type fsv34
-python run.py train/nn-mlp.ipynb --fs-type int95
-python run.py train/nn-mlp.ipynb --fs-type int96
-python run.py train/nn-mlp.ipynb --fs-type int97
-python run.py train/nn-mlp.ipynb --fs-type int98
-python run.py train/nn-mlp.ipynb --fs-type int99
-python run.py train/nn-mlp.ipynb --fs-type uni80
-python run.py train/nn-mlp.ipynb --fs-type uni85
-python run.py train/nn-mlp.ipynb --fs-type uni90
+# python run.py train/nn-mlp.ipynb --fs-type full
+# python run.py train/nn-mlp.ipynb --fs-type fsv2
+# python run.py train/nn-mlp.ipynb --fs-type fsv3
+# python run.py train/nn-mlp.ipynb --fs-type fsv4
+# python run.py train/nn-mlp.ipynb --fs-type fsv23
+# python run.py train/nn-mlp.ipynb --fs-type fsv24
+# python run.py train/nn-mlp.ipynb --fs-type fsv34
+# python run.py train/nn-mlp.ipynb --fs-type int95
+# python run.py train/nn-mlp.ipynb --fs-type int96
+# python run.py train/nn-mlp.ipynb --fs-type int97
+# python run.py train/nn-mlp.ipynb --fs-type int98
+# python run.py train/nn-mlp.ipynb --fs-type int99
+# python run.py train/nn-mlp.ipynb --fs-type uni80
+# python run.py train/nn-mlp.ipynb --fs-type uni85
+# python run.py train/nn-mlp.ipynb --fs-type uni90
 python run.py train/nn-mlp.ipynb --fs-type uni95
+
+# python run.py train/lightgbm.ipynb --fs-type full
+# python run.py train/lightgbm.ipynb --fs-type fsv2
+# python run.py train/lightgbm.ipynb --fs-type fsv3
+# python run.py train/lightgbm.ipynb --fs-type fsv4
+# python run.py train/lightgbm.ipynb --fs-type fsv23
+# python run.py train/lightgbm.ipynb --fs-type fsv24
+# python run.py train/lightgbm.ipynb --fs-type fsv34
+# python run.py train/lightgbm.ipynb --fs-type int95
+# python run.py train/lightgbm.ipynb --fs-type int96
+# python run.py train/lightgbm.ipynb --fs-type int97
+# python run.py train/lightgbm.ipynb --fs-type int98
+# python run.py train/lightgbm.ipynb --fs-type int99
+# python run.py train/lightgbm.ipynb --fs-type uni80
+# python run.py train/lightgbm.ipynb --fs-type uni85
+# python run.py train/lightgbm.ipynb --fs-type uni90
+# python run.py train/lightgbm.ipynb --fs-type uni95
