5331306f2e497ff85d88567933c70e5216c64b25
Martín Villanueva <nallivam@gmail.com>
2024-11-29 01:25:28 -0500

    ready to rerun

--- exec.sh
+++ exec.sh
@@ -1,36 +1,54 @@
 #!/bin/bash
-# python run.py train/nn-mlp.ipynb --fs-type full
-# python run.py train/nn-mlp.ipynb --fs-type fsv2
-# python run.py train/nn-mlp.ipynb --fs-type fsv3
-# python run.py train/nn-mlp.ipynb --fs-type fsv4
-# python run.py train/nn-mlp.ipynb --fs-type fsv23
-# python run.py train/nn-mlp.ipynb --fs-type fsv24
-# python run.py train/nn-mlp.ipynb --fs-type fsv34
-# python run.py train/nn-mlp.ipynb --fs-type int95
-# python run.py train/nn-mlp.ipynb --fs-type int96
-# python run.py train/nn-mlp.ipynb --fs-type int97
-# python run.py train/nn-mlp.ipynb --fs-type int98
-# python run.py train/nn-mlp.ipynb --fs-type int99
-# python run.py train/nn-mlp.ipynb --fs-type uni80
-# python run.py train/nn-mlp.ipynb --fs-type uni85
-# python run.py train/nn-mlp.ipynb --fs-type uni90
-# python run.py train/nn-mlp.ipynb --fs-type uni95
-
+set +e  # This tells bash to continue even if commands fails
+################################################################################
+python run.py train/nn-gandalf.ipynb --fs-type full
+python run.py train/nn-gandalf.ipynb --fs-type fsv2
+python run.py train/nn-gandalf.ipynb --fs-type fsv3
+python run.py train/nn-gandalf.ipynb --fs-type fsv4
+python run.py train/nn-gandalf.ipynb --fs-type fsv23
+python run.py train/nn-gandalf.ipynb --fs-type fsv24
+python run.py train/nn-gandalf.ipynb --fs-type fsv34
+python run.py train/nn-gandalf.ipynb --fs-type uni80
+python run.py train/nn-gandalf.ipynb --fs-type uni85
+python run.py train/nn-gandalf.ipynb --fs-type uni90
+python run.py train/nn-gandalf.ipynb --fs-type uni95
+################################################################################
+python run.py train/nn-1dcnn.ipynb --fs-type full
+python run.py train/nn-1dcnn.ipynb --fs-type fsv2
+python run.py train/nn-1dcnn.ipynb --fs-type fsv3
+python run.py train/nn-1dcnn.ipynb --fs-type fsv4
+python run.py train/nn-1dcnn.ipynb --fs-type fsv23
+python run.py train/nn-1dcnn.ipynb --fs-type fsv24
+python run.py train/nn-1dcnn.ipynb --fs-type fsv34
+python run.py train/nn-1dcnn.ipynb --fs-type uni80
+python run.py train/nn-1dcnn.ipynb --fs-type uni85
+python run.py train/nn-1dcnn.ipynb --fs-type uni90
 python run.py train/nn-1dcnn.ipynb --fs-type uni95
-
-# python run.py train/lightgbm.ipynb --fs-type full
-# python run.py train/lightgbm.ipynb --fs-type fsv2
-# python run.py train/lightgbm.ipynb --fs-type fsv3
-# python run.py train/lightgbm.ipynb --fs-type fsv4
-# python run.py train/lightgbm.ipynb --fs-type fsv23
-# python run.py train/lightgbm.ipynb --fs-type fsv24
-# python run.py train/lightgbm.ipynb --fs-type fsv34
-# python run.py train/lightgbm.ipynb --fs-type int95
-# python run.py train/lightgbm.ipynb --fs-type int96
-# python run.py train/lightgbm.ipynb --fs-type int97
-# python run.py train/lightgbm.ipynb --fs-type int98
-# python run.py train/lightgbm.ipynb --fs-type int99
-# python run.py train/lightgbm.ipynb --fs-type uni80
-# python run.py train/lightgbm.ipynb --fs-type uni85
-# python run.py train/lightgbm.ipynb --fs-type uni90
-# python run.py train/lightgbm.ipynb --fs-type uni95
+################################################################################
+python run.py train/nn-mlp.ipynb --fs-type full
+python run.py train/nn-mlp.ipynb --fs-type fsv2
+python run.py train/nn-mlp.ipynb --fs-type fsv3
+python run.py train/nn-mlp.ipynb --fs-type fsv4
+python run.py train/nn-mlp.ipynb --fs-type fsv23
+python run.py train/nn-mlp.ipynb --fs-type fsv24
+python run.py train/nn-mlp.ipynb --fs-type fsv34
+python run.py train/nn-mlp.ipynb --fs-type uni80
+python run.py train/nn-mlp.ipynb --fs-type uni85
+python run.py train/nn-mlp.ipynb --fs-type uni90
+python run.py train/nn-mlp.ipynb --fs-type uni95
+################################################################################
+python run.py train/catboost.ipynb --fs-type full
+python run.py train/catboost.ipynb --fs-type fsv2
+python run.py train/catboost.ipynb --fs-type fsv3
+python run.py train/catboost.ipynb --fs-type fsv4
+python run.py train/catboost.ipynb --fs-type fsv23
+python run.py train/catboost.ipynb --fs-type fsv24
+python run.py train/catboost.ipynb --fs-type fsv34
+python run.py train/catboost.ipynb --fs-type uni80
+python run.py train/catboost.ipynb --fs-type uni85
+python run.py train/catboost.ipynb --fs-type uni90
+python run.py train/catboost.ipynb --fs-type uni95
+################################################################################
+python run.py train/catboost_text.ipynb --fs-type full
+python run.py train/catboost_text.ipynb --fs-type uni90
+################################################################################
